95217ea144bc6c6e48b85ba300664c28fb0fe104
LegoTed2000 <112373501+LegoTed2000@users.noreply.github.com>
2023-10-30 17:00:05 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ function preload() {
 function setup() {
     canvas = createCanvas(900, 700);
     canvas.center();
+    objectDetector = ml5.objectDetector("cocossd", modleLoaded);
 }
 
 function draw() {
@@ -16,4 +17,29 @@ function draw() {
     stroke("#fc0303");
     rect(350, 60, 500, 450);
     strokeWeight(3);
+
+    fill("#0238fa");
+    text("D  o  g", 120, 80);
+    noFill();
+    stroke("#0238fa");
+    rect(100, 60, 500, 600)
+
+    fill("#02fa17");
+    text("B  o  w  l", 390, 500);
+    noFill();
+    stroke("#02fa17");
+    rect(370, 480, 200, 200)
+}
+
+function modleLoaded() {
+    console.log("COCOSSD HAS LOADED SSSSSSSSSSSSSSSSS");
+    Status = true;
+    objectDetector.detect(img, gotResults);
+}
+
+function gotResults(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
 }
